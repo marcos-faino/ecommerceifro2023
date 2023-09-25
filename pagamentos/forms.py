f46d116c83cc6a0629b8b58f42d923c802d3e9c4
Marcos Alves Faino <1812293@ifro.local>
2023-09-25 19:07:11 -0400

    até 19-09

---
+++ pagamentos/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+
+class CheckoutForm(forms.Form):
+    payment_method_nonce = forms.CharField(max_length=500, widget=forms.widgets.HiddenInput, required=False)
+
+    def clean(self):
+        self.cleaned_data = super().clean()
+        if not self.cleaned_data.get('payment_method_nonce'):
+            raise forms.ValidationError('Não foi possível processar o pagamento. Tente novamente.')
+        return self.cleaned_data
